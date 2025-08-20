be36c9d3fd573912c305e5059fae7b0097bd10c2
syahrulannam <syahrulot132@gmail.com>
2025-08-20 10:52:34 +0700

    Remove sensitive file from tracking

--- src/hash_password.js
+++
@@ -1,7 +0,0 @@
-import bcrypt from "bcrypt";
-export const newPassword = "syahrulmind"; // Ganti dengan kata sandi baru Anda
-
-bcrypt.hash(newPassword, 10, function (err, hash) {
-  if (err) throw err;
-  console.log(hash);
-});
